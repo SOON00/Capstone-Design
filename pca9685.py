8e387ae23fb9a65582d443f83eb83f45fafc3a65
soon <gayadoli@daum.net>
2024-03-04 10:25:10 +0000

    pca9685.py

---
+++ pca9685.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import time
+import Adafruit_PCA9685
+import rospy
+from std_msgs.msg import String
+
+robot_handle=Adafruit_PCA9685.PCA9685()
+
+servoMin=150
+servoMax=650
+
+def map(value,min_angle,max_angle,min_pulse,max_pulse):
+    angle_range=max_angle-min_angle
+    pulse_range=max_pulse-min_pulse
+    scale_factor=float(angle_range)/float(pulse_range)
+    return min_pulse+(value/scale_factor)
+    
+def set_angle(channel,angle):
+    pulse=int(map(angle,0,180,servoMin,servoMax))
+    robot_handle.set_pwm(channel,0,pulse)
+    
+def msg_callback(msg):
+    try:
+        value = int(msg.data)
+        value = int((value-1000)/10)
+        set_angle(0, value)
+        set_angle(1, value)
+        set_angle(2, value)
+        set_angle(3, value)
+        print(value)
+    except ValueError:
+        print("error")
+
+robot_handle.set_pwm_freq(50)        
+
+'''
+set_angle(0,100)
+set_angle(1,100)
+set_angle(2,100)
+set_angle(3,100)
+print('100')
+time.sleep(10)
+
+set_angle(0,0)
+set_angle(1,0)
+set_angle(2,0)
+set_angle(3,0)
+print('0')
+time.sleep(5)
+'''
+
+if __name__ == '__main__':
+    rospy.init_node('topic_sub_node')
+    sub = rospy.Subscriber('/chatter',String,msg_callback,queue_size=1)
+    
+    rospy.spin()
