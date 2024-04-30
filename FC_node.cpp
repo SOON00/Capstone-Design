ac556b0fa742edf1df2e5f6e0231b1d81c3c207a
soon <gayadoli@daum.net>
2024-04-30 11:21:01 +0000

    FC_node.cpp

--- FC_node.cpp
+++ FC_node.cpp
@@ -10,20 +10,58 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Float32.h>
 
-double freq=200;//controller loop frequency
+const double PI = 3.1415926;
 
-//RC_readings
-int arr[7];//0:roll, 1:pitch, 2:yaw, 3:thrust, 4:3-step switch 이거는 조종기 추가적인 스위치/버튼 인듯
+class dualPIDController{
+private:
+    double angle_Kp;
+    double angle_Ki;
+    double angle_Kd;
 
+    double rate_Kp;
+    double rate_Ki;
+    double rate_Kd;
+    
+    double prevError;
+    double angle_integral;
+    double rate_integral;
+public:
+    dualPIDController(double angle_p, double angle_i, double angle_d, double rate_p, double rate_i, double rate_d) :
+    angle_Kp(angle_p), angle_Ki(angle_i), angle_Kd(angle_d), rate_Kp(rate_p), rate_Ki(rate_i), rate_Kd(rate_d), prevError(0), angle_integral(0), rate_integral(0) {}
+    double calculate(double target, double angle_input, double rate_input, double dt){
+        double angle_error = target - angle_input;
+        
+        double angle_P_term = angle_Kp*angle_error;
+        double target_rate = angle_P_term;
+        
+        double rate_error = target_rate - rate_input;
+        
+        double rate_P_term = rate_Kp*rate_error;
+        
+        rate_integral += rate_error*dt;
+        double rate_I_term = rate_Ki * rate_integral;
+        
+        double rate_D_term = rate_Kd * (rate_error-prevError)/dt;
+        
+        angle_integral += angle_error*dt;
+        double angle_I_term = angle_Ki * angle_integral;
+        
+        double angle_D_term = angle_Kd * rate_input;
+        
+        prevError = rate_error;
+        
+        
+        double controlOutput = rate_P_term + rate_I_term + rate_D_term + angle_I_term + angle_D_term;
+        
+        return controlOutput; 
+    }
+};//dualPID control class
+
+double freq=200;//controller loop frequency 0.005s
+std_msgs::Int16MultiArray PWM_cmd;
+
+int arr[7];//0:roll, 1:pitch, 2:yaw, 3:thrust, 4:3-step switch 
 float imu_array[6];
-// imu_array[0] roll angle
-// imu_array[1] pitch angle
-// imu_array[2] yaw angle
-// imu_array[3] roll angular velocity
-// imu_array[4] pitch angular velocity
-// imu_array[5] yaw angular velocity
-
-int devo_data;
 
 //Commands================================================
 
@@ -38,8 +76,6 @@ double e_p_i=0;//pitch error integration
 
 double tau_r_d=0;//roll  desired torque (N.m)         여기에 PID제어 들어감
 double tau_p_d=0;//pitch desired torque(N.m)          여기도 마찬가지
-
-std_msgs::Int16MultiArray PWM_cmd;
 
 //ud_cmd
 double r_d=0;//desired roll angle             목표 롤값
@@ -62,15 +98,12 @@ static double yaw_limit=0;
 
 
 //Function declaration====================================
-
 void arrayCallback(const std_msgs::Float32MultiArray::ConstPtr &array);
+void imu_arrayCallback(const std_msgs::Float32MultiArray::ConstPtr &array);
+
 void ud_to_PWM(double tau_r_des, double tau_p_des, double tau_y_des, double Thrust_des);
 double Force_to_PWM(double F, double Thrust);
 void rpyT_ctrl(double roll_d, double pitch_d, double yaw_d, double Thrust_d);
-
-void imu_arrayCallback(const std_msgs::Float32MultiArray::ConstPtr &array);
-
-void msg_callback(const std_msgs::Float32::ConstPtr &msg);
 
 double constrain(double F);
 //--------------------------------------------------------
@@ -91,16 +124,13 @@ double Dp=0;
 double Par=0.01;//angular roll
 double Pap=0.015;//angular pitch
 
-//Roll, Pitch PID gains (FP fix mode)
-double Pa_fp=3;
-double Ia_fp=0.01;
-double Da_fp=0.5;
-
 //Yaw PID gains
 double Py=1;//1 : good
 double Dy=0;
-//--------------------------------------------------------
 
+dualPIDController tau_Roll(2,0,0,1,0,0);
+dualPIDController tau_Pitch(2,0,0,1,0,0);
+//--------------------------------------------------------
 
 //Main====================================================
 
@@ -112,9 +142,7 @@ int main(int argc, char **argv){
 	ros::Publisher PWM=nh.advertise<std_msgs::Int16MultiArray>("PWM", 100);
 
 	//Subscriber
-
 	ros::Subscriber imu=nh.subscribe("/imu", 100, &imu_arrayCallback);
-
 	ros::Subscriber devo=nh.subscribe("/PPM", 100, &arrayCallback);
 
 	ros::Rate loop_rate(200);
@@ -126,12 +154,12 @@ int main(int argc, char **argv){
 		 	flag_imu=0;
 
 		 	PWM_cmd.data.resize(4);
-		 	PWM_cmd.data[0]=100;//각 모터에 대한 PWM값
-		 	PWM_cmd.data[1]=100;
-		 	PWM_cmd.data[2]=100;
-		 	PWM_cmd.data[3]=100;
+		 	PWM_cmd.data[0]=200;//각 모터에 대한 PWM값
+		 	PWM_cmd.data[1]=200;
+		 	PWM_cmd.data[2]=200;
+		 	PWM_cmd.data[3]=200;
 		}
-
+		
 		 else{//5번 스위치 작동
 			//Initialize desired yaw
 			if(flag_imu!=1){
@@ -143,7 +171,6 @@ int main(int argc, char **argv){
 				flag_imu=1;
                 //드론의 수직 Z축 방향 선형 가속도가 거의 0일 때 imu 작동하게
 			}
-	
 			//TGP ctrl
 			r_d=rp_limit*(-(arr[4]-(double)1500)/(double)500);
             //목표 롤 각도 최대값 곱하기 -1or1 (수신기 신호를 -1~1로 맵핑하는 느낌)
@@ -170,13 +197,13 @@ int main(int argc, char **argv){
 		}
 			if(fabs(imu_array[0])>35 || fabs(imu_array[1])>35) { // Emergency Stop
 		 	PWM_cmd.data.resize(4);
-		 	PWM_cmd.data[0]=100;//각 모터에 대한 PWM값
-		 	PWM_cmd.data[1]=100;
-		 	PWM_cmd.data[2]=100;
-		 	PWM_cmd.data[3]=100;
+		 	PWM_cmd.data[0]=200;//각 모터에 대한 PWM값
+		 	PWM_cmd.data[1]=200;
+		 	PWM_cmd.data[2]=200;
+		 	PWM_cmd.data[3]=200;
 		 	PWM.publish(PWM_cmd);
 		 	break;             //Emergency break code
-		}
+		       }
 			/*if(arr[4] <= 100) { // Emergency Stop
 		 	PWM_cmd.data.resize(4);
 		 	PWM_cmd.data[0]=100;//각 모터에 대한 PWM값
@@ -212,13 +239,6 @@ void imu_arrayCallback(const std_msgs::Float32MultiArray::ConstPtr &array){
 	return;
 }
 
-void msg_callback(const std_msgs::Float32::ConstPtr &msg){
-	devo_data = msg->data;
-	return;
-}
-int PP=0;
-double pitch_D=0;
-double prev_pitch_D=0;
 void rpyT_ctrl(double roll_d, double pitch_d, double yaw_d, double Thrust_d){
 //원하는 rpy,thrust값 받아서 PID제어. 롤,피치의 명령 토크 계산. 
 //롤,피치 명령 토크와 요 각도 편차를 이용하여 요 명령 토크 계산
@@ -230,19 +250,6 @@ void rpyT_ctrl(double roll_d, double pitch_d, double yaw_d, double Thrust_d){
 	double angular_velocity_pitch=imu_array[4];
 	
 	double rate_error = Pr*e_r-angular_velocity_roll;
-	
-	
-	/*
-	PP=arr[6];
-	
-	if(PP<1100) PP=1100;
-	if(PP>1900) PP=1900;
-	
-	PP-=1100;
-	PP/=50;//0~16
-	
-	Pr=0.06 + (float)PP/100;
-	//*/
 	
 	if(e_y>180) e_y-=360;
 	else if (e_y<-180) e_y+=360;
@@ -260,28 +267,27 @@ void rpyT_ctrl(double roll_d, double pitch_d, double yaw_d, double Thrust_d){
 	tau_r_d=0;//롤 방향 목표 토크
 	tau_p_d=0;//피치 방향 목표 토크
 
-		//일반적인 PID 제어
-	//tau_r_d=Pr*e_r+Ia*e_r_i+Da*(-imu_array[3])+(double)0.3;
+		//PID 제어
 	//tau_r_d=Pr*e_r+Ia*e_r_i+Da*(imu_array[3]);
-	//tau_r_d=Par*(Pr*e_r-angular_velocity_roll)+Ia*e_r_i+Da*(imu_array[3]);
-	tau_r_d=Par*(Pr*e_r-angular_velocity_roll)+Ia*e_r_i;
+	tau_r_d=Par*(Pr*e_r-angular_velocity_roll)+Ia*e_r_i+Da*(imu_array[3]);
 	
         	//롤 각도에 대한 목표 토크 PID로 구하기
         	
-	//tau_p_d=Pp*e_p+Ia*e_p_i+Dp*(-imu_array[4])+(double)0.2;
 	//tau_p_d=Pp*e_p+Ia*e_p_i+Dp*(-imu_array[4]);
 	tau_p_d=Pap*(Pp*e_p+angular_velocity_pitch)+Ia*e_p_i+Dp*(-imu_array[4]);
 
 	double tau_y_d=Py*e_y+Dy*(-imu_array[5]);
+	
+	double tau_Roll_input = tau_Roll.calculate(0, imu_array[0]*PI/180, imu_array[3]*PI/180,0.005);
+	double tau_Pitch_input = tau_Pitch.calculate(0, imu_array[1]*PI/180, -imu_array[4]*PI/180,0.005);
 
-	//ROS_INFO("xvel:%lf, yvel:%lf, zvel:%lf", TGP_ang_vel.x, TGP_ang_vel.y, TGP_ang_vel.z);
-	//ROS_INFO("tr:%lf, tp:%lf, ty:%lf, Thrust_d:%lf", tau_r_d, tau_p_d, tau_y_d, Thrust_d);
-	ROS_INFO("Pa:%lf",Pr);
-	ud_to_PWM(tau_r_d, tau_p_d, tau_y_d, Thrust_d);
+	//ROS_INFO("Roll :%lf, Pitch :%lf, ty:%lf, Thrust_d:%lf", tau_r_d, tau_p_d, tau_y_d, Thrust_d);
+	ROS_INFO("roll :%lf, pitch :%lf", tau_Roll_input, tau_Pitch_input);
+	//ud_to_PWM(tau_r_d, tau_p_d, tau_y_d, Thrust_d);
+	ud_to_PWM(tau_Roll_input, tau_Pitch_input, tau_y_d, Thrust_d);//tau_r_d,tau_p_d
 }
 
-void ud_to_PWM(double tau_r_des, double tau_p_des, double tau_y_des, double Thrust_des){
-	
+void ud_to_PWM(double tau_r_des, double tau_p_des, double tau_y_des, double Thrust_des){	
 	F1 = -(1.5625 * tau_r_des + 1.5625 * tau_p_des - 0.25 * tau_y_des - 0.25 * Thrust_des);
 	F2 = -(1.5625 * tau_r_des - 1.5625 * tau_p_des + 0.25 * tau_y_des - 0.25 * Thrust_des);
 	F3 = -(-1.5625 * tau_r_des - 1.5625 * tau_p_des - 0.25 * tau_y_des - 0.25 * Thrust_des);
@@ -290,7 +296,7 @@ void ud_to_PWM(double tau_r_des, double tau_p_des, double tau_y_des, double Thru
 	F2= constrain(F2);
 	F3= constrain(F3);
 	F4= constrain(F4);
-	ROS_INFO("F1:%lf, F2:%lf, F3:%lf, F4:%lf", F1, F2, F3, F4);
+	//ROS_INFO("F1:%lf, F2:%lf, F3:%lf, F4:%lf", F1, F2, F3, F4);
 	PWM_cmd.data.resize(4);
 	PWM_cmd.data[0]=Force_to_PWM(F1,Thrust_des);
 	PWM_cmd.data[1]=Force_to_PWM(F2,Thrust_des);
@@ -313,4 +319,3 @@ double Force_to_PWM(double F, double Thrust){
 
 	return pwm;
 }
-//--------------------------------------------------------
